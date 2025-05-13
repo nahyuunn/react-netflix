a6dc618f8f5770914f7c1531f6bbcaaa2284f617
nahyuunn <138644012+nahyuunn@users.noreply.github.com>
2025-05-13 15:28:35 +0900

    initial

---
+++ src/components/Nav.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
+import { useNavigate } from "react-router-dom";
+
+export default function Nav() {
+  const [show, setShow] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      console.log("window.scrollY", window.scrollY);
+      if (window.scrollY > 50) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    });
+    return () => {
+      window.removeEventListener("scroll", () => {});
+    };
+  }, []);
+
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+    navigate(`/search?q=${e.target.value}`);
+  };
+
+  return (
+    <nav className={`nav ${show && "nav__black"}`}>
+      <img
+        alt="Netflix logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+        className="nav__logo"
+        onClick={() => window.location.reload()}
+      />
+
+      <input
+        value={searchValue}
+        onChange={handleChange}
+        className="nav__input"
+        type="text"
+        placeholder="영화를 검색해주세요."
+      />
+      <img
+        alt="User logged"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+        className="nav__avatar"></img>
+    </nav>
+  );
+}
